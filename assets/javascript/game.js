6a7bf690b1cddace5b05c10da8be345ffc74ddcf
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-06-16 15:23:44 -0400

    savepoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,76 @@
 $(document).ready(function() {
 
-var i = 0;
+// global variables
 
-$("body").on("click", function() {
+    var i = 0;
+    var btnIsActive = false;
+    var noMoreCards = false;
 
+// question bank
+
+    var qBank = [
+
+    {
+        question: "<b>Test Question 1</b>",
+        answers: "<ul><li>answer A</li> <li>answer B</li> <li>answer C</li></ul>",
+        correct: "A"
+    },
+
+    {
+        question: "<b>Text Question 2</b>",
+        answers: "<ul><li>answer A</li> <li>answer B</li> <li>answer C</li></ul>",
+        correct: "B"
+    }
+
+    ]
+
+// global functions
+
+
+// TEMP
+$("body").on("click", function(){
     nextCard();
-
 })
 
 function nextCard() {
-
+    
     var cardThree = $("#div-three > .card");
     var cardTwo = $("#div-two > .card");
-    var cardFront = $("#div-front > .card");
+    var cardFront = $("#div-front > .card"); 
     
-    $("#card-alpha").clone()
-        .addClass("1")
-        .html("<h1>" + i + "</h1>")
+    if(!noMoreCards) {
+        // clone the prototype card and put it in back position
+        $("#card-alpha").clone().empty()
+        .addClass("show")
         .appendTo("#div-four")
         .animate({
             bottom: '120px',
             width: '55%',
             height: '270px'
-        }).prependTo("#div-three");
+        })
+        .attr("id", "card" + i)
+        .prependTo("#div-three");
+        
+        var q = Math.floor(i/2);
+        var id = "#card" + i;
+        var thisCard = $(id); 
 
+        if(q < qBank.length) {
+
+            // if i is even, put next question on the new card
+            if(i % 2 === 0) {
+                thisCard.append(qBank[q].question);
+                thisCard.append(qBank[q].answers);
+            } 
+
+        } else {
+            // there are no questions left in the bank
+            thisCard.append("<span>Final Stats</span>");
+            noMoreCards = true;
+        }        
+    }
+
+    // move cards forward
     cardThree.animate({
         bottom: '65px',
         width: '75%',
@@ -46,6 +93,42 @@ function nextCard() {
     }
 
     i++;
+
+    if(btnIsActive === true) {
+        btnIsActive = false;
+    } else {btnIsActive = true;}
+    console.log(btnIsActive);
 }
+
+function endGame() {
+    
+}
+
+// click listeners
+
+$(".button").on("click", function() {
+
+    if(btnIsActive === true) {
+        
+        var q = Math.floor((i-3)/2);
+        var answerCard = $("#div-two > .card");
+
+        if($(this).attr("data-response") === qBank[q].correct) {
+            answerCard.append("<span>That's right!</span>");
+        } else {
+            answerCard.append("<span>That's wrong!</span>");
+        }
+
+        nextCard();
+    }
+})
+
+// execute
+
+nextCard();
+nextCard();
+nextCard();
+
+
 
 })
